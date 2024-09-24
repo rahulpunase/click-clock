c6e0a6009f8585e7d4212c93b190f56eae2a34b7
rahulpunase <rahulpunase@gmail.com>
2024-09-24 22:23:43 +0530

    fix: [file Input.tsx]

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { visualizer } from "rollup-plugin-visualizer";
+import webpackStatsPlugin from "rollup-plugin-webpack-stats";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
@@ -8,10 +9,16 @@ export default defineConfig({
   plugins: [
     react(),
     visualizer({ open: true, filename: "bundle-visualization.html" }),
+    webpackStatsPlugin(),
   ],
   build: {
     rollupOptions: {
       treeshake: true,
+      output: {
+        assetFileNames: "assets/[name].[hash][extname]",
+        chunkFileNames: "assets/[name].[hash].js",
+        entryFileNames: "assets/[name].[hash].js",
+      },
     },
   },
   resolve: {
