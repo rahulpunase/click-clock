b2401efa8600364a1c9d4f4204d72c26e424e04c
rahulpunase <rahulpunase@gmail.com>
2024-09-30 22:42:30 +0530

    changes

---
+++ tests/utils.ts
@@ -0,0 +1,12 @@
+import { Page } from "@playwright/test";
+import { loadEnv } from "vite";
+
+const env = loadEnv("development", process.cwd());
+const baseHostUrl = `${env.VITE_LOCALHOST}:${env.VITE_PORT}/`;
+
+export const urlUtils = (page: Page) => {
+  return {
+    visitSite: async () => await page.goto(baseHostUrl),
+    visitUrl: async (url: string) => await page.goto(`${baseHostUrl}${url}`),
+  };
+};
