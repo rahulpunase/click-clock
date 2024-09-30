afb5af8418930121101ecf4a0b5f97e07c425426
rahulpunase <rahulpunase@gmail.com>
2024-09-30 23:17:25 +0530

    Merging to staging.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster } from "@/design-system/ui/Toast/Toaster";
 import { TooltipProvider } from "@/design-system/ui/Tooltip/Tooltip";
 
 function App() {
+  console.log("staging");
   return (
     <TooltipProvider delayDuration={50}>
       <AppRoutes />
