7623a01b25f12785dc88992e9718d0c61c073d04
rahulpunase <rahulpunase@gmail.com>
2024-10-02 21:27:38 +0530

    change

--- playwright.config.ts
+++ playwright.config.ts
@@ -78,9 +78,9 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
 
-  webServer: {
-    command: "npm run dev",
-    url: baseHostUrl,
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //   command: "npm run dev",
+  //   url: baseHostUrl,
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
